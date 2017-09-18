52418ec7902fb04a7f5f2a7b50231e83f0aa5527
Erik-jan Riemers <riemers@binkey.nl>
2017-09-19 00:17:20 +0200

    Update main.py
    Added frontdoor open option

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 ha_api_url = os.getenv('HA_API_URL', 'http://localhost:8123/api')
 ha_doorbell_entity = os.getenv('HA_DOORBELL_ENTITY', 'binary_sensor.doorbell')
 ha_frontdoormotion_entity = os.getenv('HA_FRONTDOORMOTION_ENTITY', 'binary_sensor.frontdoor_motion')
+ha_frontdooropen_entity = os.getenv('HA_FRONTDOOROPEN_ENTITY', 'binary_sensor.frontdoor_open')
 
 @app.route("/")
 def default():
@@ -43,6 +44,31 @@ def doorbell():
 def frontdoormotion():
     headers = {'content-type': 'application/json'}
     url = '{}/states/{}'.format(ha_api_url, ha_frontdoormotion_entity)
+
+    res = requests.get(url, headers=headers)
+    if res.status_code != 200:
+        return res.content, res.status_code
+
+    entity = res.json()
+
+    data = {'state': 'on', 'attributes': entity['attributes']}
+    res  = requests.post(url, headers=headers, data=json.dumps(data))
+    if res.status_code != 200:
+        return res.content, res.status_code
+
+    time.sleep(1)
+
+    data = {'state': 'off', 'attributes': entity['attributes']}
+    res  = requests.post(url, headers=headers, data=json.dumps(data))
+    if res.status_code != 200:
+        return res.content, res.status_code
+
+    return "OK", res.status_code
+
+@app.route("/frontdooropen")
+def frontdooropen():
+    headers = {'content-type': 'application/json'}
+    url = '{}/states/{}'.format(ha_api_url, ha_frontdooropen_entity)
 
     res = requests.get(url, headers=headers)
     if res.status_code != 200:
